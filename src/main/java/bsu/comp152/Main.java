027ee0aec9f578c5fc983c8c72fe2a7c7aeff571
ericlessantostv <ericlessantos1999@gmail.com>
2019-11-26 00:08:04 -0500

    Creating window

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,7 +1,14 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -11,6 +18,19 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Parent root = null;
+        var loc = getClass().getResource("Main.fxml");
+        try{
+            root = FXMLLoader.load(loc);
+        } catch (IOException e) {
+            System.out.println("Unable to find the FXML file");
+            e.printStackTrace();
+        }
+        Scene windowContents = new Scene(root, 300, 400);
+        primaryStage.setScene(windowContents);
+        primaryStage.setTitle("Project 5");
+        primaryStage.show();
 
     }
+
 }
