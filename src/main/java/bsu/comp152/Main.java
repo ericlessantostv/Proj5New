c9ea12d739f576ad040869d96e9a6c6947cc4b9a
ericlessantostv <ericlessantos1999@gmail.com>
2019-12-05 11:33:00 -0500

    Updated

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,12 +1,16 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,14 +24,6 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         VBox box = new VBox();
         box.setAlignment(Pos.CENTER);
-        Parent root = null;
-        var loc = getClass().getResource("Main.fxml");
-        try{
-            root = FXMLLoader.load(loc);
-        } catch (IOException e) {
-            System.out.println("Unable to find the FXML file");
-            e.printStackTrace();
-        }
         Scene windowContents = new Scene(box, 300, 400);
         primaryStage.setScene(windowContents);
         primaryStage.setTitle("Project 5");
@@ -41,9 +37,37 @@ public class Main extends Application {
         button2.setPrefSize(130,30);
         button2.setTranslateY(-70);
         box.getChildren().add(button2);
+        /*
+        EventHandler<javafx.event.ActionEvent> handler = new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                VBox starBox = new VBox();
+                Stage starWindow = new Stage();
+                Scene newContents = new Scene(starBox, 300, 400);
+                starWindow.setScene(newContents);
+                starWindow.setTitle("Star Wars");
+                starWindow.show();
+            }
+        };
 
+         */
+        button2.setOnAction(this::openStarsWarsWindow);
 
-
+    }
+    @FXML
+    public  void openStarsWarsWindow(ActionEvent event){
+        Parent root = null;
+        var loc = getClass().getResource("StarWarsWindow.fxml");
+        try {
+            root = FXMLLoader.load(loc);
+        }catch (IOException e){
+            System.out.println("Couldn't Find FXML file!!!!!!");
+        }
+        Scene windowContents = new Scene(root, 900,400);
+        Stage recipeWindow = new Stage();
+        recipeWindow.setScene(windowContents);
+        recipeWindow.setTitle("Star Wars");
+        recipeWindow.show();
     }
 
 }
