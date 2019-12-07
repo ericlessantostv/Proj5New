9f2fa23535fa38a33652731cc78df434ce16a565
seannauss1 <54951263+seannauss1@users.noreply.github.com>
2019-12-07 16:27:44 -0600

    Controller & DataHandler made with RadioButtons to select joke. Still doesnt connect to API website but layout starting to be made

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,6 +1,8 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -16,7 +18,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-@Override
+    @Override
     public void start(Stage primaryStage) {
         VBox box = new VBox();
         box.setAlignment(Pos.CENTER);
@@ -37,7 +39,70 @@ public class Main extends Application {
         button1.setPrefSize(130,30);
         button1.setTranslateY(-100);
         box.getChildren().add(button1);
+        button1.setOnAction(this::openMakeupWindow);
+        Button button2 = new Button("Star Wars");
+        button2.setPrefSize(130,30);
+        button2.setTranslateY(-70);
+        box.getChildren().add(button2);
+        button2.setOnAction(this::openStarWarsWindow);
+        Button button3 = new Button("JokeAPI");
+        button3.setPrefSize(130,30);
+        button3.setTranslateY(-70);
+        box.getChildren().add(button3);
+        button3.setOnAction(this::openJokeAPI);
 
+
+
+    }
+    @FXML
+    public  void openMakeupWindow(ActionEvent event){
+        Parent root = null;
+        var loc = getClass().getResource("MakeupWindow.fxml");
+        try {
+            root = FXMLLoader.load(loc);
+        }catch (IOException e){
+            System.out.println("Couldn't Find FXML file!!!!!!");
+        }
+        Scene windowContents = new Scene(root, 900,400);
+        Stage recipeWindow = new Stage();
+        recipeWindow.setScene(windowContents);
+        recipeWindow.setTitle("Makeup");
+        recipeWindow.show();
+    }
+    @FXML
+    public  void openStarWarsWindow(ActionEvent event){
+        Parent root = null;
+        var loc = getClass().getResource("StarWarsWindow.fxml");
+        try {
+            root = FXMLLoader.load(loc);
+        }catch (IOException e){
+            System.out.println("Couldn't Find FXML file!!!!!!");
+        }
+        Scene windowContents = new Scene(root, 900,400);
+        Stage recipeWindow = new Stage();
+        recipeWindow.setScene(windowContents);
+        recipeWindow.setTitle("Star Wars");
+        recipeWindow.show();
+    }
+
+
+
+
+    public void openJokeAPI(ActionEvent event) {
+        Parent root = null;
+        var loc = getClass().getResource("Main.fxml");
+        try {
+            root = FXMLLoader.load(loc);
+
+        }
+        catch (IOException e) {
+            System.out.println("No FXML File");
+        }
+        Scene jokeContents = new Scene(root, 300, 400);
+        Stage jokeWindow = new Stage();
+        jokeWindow.setScene(jokeContents);
+        jokeWindow.setTitle("Showing Data");
+        jokeWindow.show();
     }
 
 }
